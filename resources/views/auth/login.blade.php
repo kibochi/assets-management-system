6ae3fe54e86d56180aec9092b005772c979765d7
Parrot Live user <user@localhost.localdomain>
2021-11-19 23:20:14 +0000

    changed login/register blade

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,28 +1,36 @@
 @extends('layouts.app')
 @section('content')
 
-    <div class="box">
-        <div class="box-left">
-            <div class="big-image title">
-                <h1 class="text-muted font-weight-bold">Not a Member?</h1>
-                <div class="text-center  mt-3">
-                    <a href="{{ route('register') }}" class=" btn btn-outline-dark w-100">Register Now</a>
-                </div>
-            </div>
-        </div>
 
-        <div class="box-right ">
-            <div class="form">
+<div class="row form">
+
+<div class="col-sm-6 col-md-6 col-lg-6 left">
+<div class="left-side title">
+    
+    
+                <h1 class="text-muted ftext-center ont-weight-bold">Not a Member?</h1>
+                <div class="text-center  mt-3">
+             
+                    <a href="{{ route('register') }}" class=" btn btn-outline-dark ">Register Now</a>
+                </div>
+
+</div>
+</div>
+
+<div class="col-sm-6 col-md-6 col-lg-6 right">
+<div class="right-side">
                 <div class="img text-center mb-3">
-                    <img src="{{ asset('images/lock.png') }}" class="img-responsive">
+                    <img src="{{ asset('images/locks.png') }}" class="img-responsive">
                 </div>
                 <div class="text-center title2 mb-4">
                     <h2>Admin Login</h2>
                 </div>
                 @include('forms.login_form')
             </div>
-        </div>
-    </div>
+</div>
+</div>
+
+  
 
 
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,30 +1,39 @@
 @extends('layouts.app')
-
 @section('content')
 
-    
-<div class="box">
-        <div class="box-left">
-            <div class="big-image title">
-                <h1 class="text-muted font-weight-bold">Already a Member?</h1>
-                <div class="text-center  mt-3">
-                    <a href="{{ route('login') }}" class=" btn btn-outline-dark w-50">Login</a>
-                </div>
-            </div>
-        </div>
 
-        <div class="box-right ">
-            <div class="form">
+<div class="row form">
+
+<div class="col-sm-6 col-md-6 col-lg-6 left">
+<div class="left-side title">
+    
+    
+                <h1 class="text-muted text-center font-weight-bold">Already a Member?</h1>
+                <div class="text-center  mt-3">
+                    <a href="{{ route('login') }}" class=" btn btn-outline-dark ">Login Now</a>
+                </div>
+
+</div>
+</div>
+
+<div class="col-sm-6 col-md-6 col-lg-6 right">
+<div class="register ">
                 <div class="img text-center mb-3">
-                    <img src="{{ asset('images/lock.png') }}" class="img-responsive">
+                    <img src="{{ asset('images/locks.png') }}" class="img-responsive">
                 </div>
                 <div class="text-center title2 mb-4">
                     <h2>New Account</h2>
                 </div>
                 @include('forms.register_form')
             </div>
-        </div>
-    </div>
+</div>
+</div>
+
+  
+
+
+
+
 
 
 @endsection
